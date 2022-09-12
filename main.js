5867b6f0faf2492e2961daf26584711e15fc4eb4
Meha931 <33491127+Meha931@users.noreply.github.com>
2022-09-12 15:03:42 +0700

    Tested with static files

--- main.js
+++ main.js
@@ -8,17 +8,38 @@ const website = express();
 website.set("views", root);
 website.set("view engine", "ejs");
 
+function doesFileExist(path) {
+    try {
+        readFileSync(path);
+        return true;
+    } catch (e) {
+        //console.log(e);
+        return false;
+    }
+}
+function getExt(path) {
+    let l = path.lastIndexOf(".");
+    return (l===-1) ? "" : path.slice(l+1);
+}
+
 website.get("/*", (req, res) => {
     let p = root+req.path;
-    ejs.renderFile(p, {}, {}, (err, str) => {
-        if (err) {
-            //console.log(err);
-            res.sendStatus(400);
-            return;
-        }
-        res.send(str);
-    });
+    if (getExt(req.path) === "ejs") {
+        ejs.renderFile(p, {}, {}, (err, str) => {
+            if (err) {
+                //console.log(err);
+                res.sendStatus(400);
+                return;
+            }
+            res.send(str);
+        });
+    } else {
+        if (doesFileExist(p)) res.sendFile(__dirname+"/"+p);
+        else res.sendStatus(404);
+    }
 });
+
+//website.use(express.static(__dirname+"/"+root));
 
 website.listen(13377, () => {
     console.log("Ready");
