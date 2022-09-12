7b2c6f069bd5f44526bf6379f4401adf8d3632b2
Meha931 <33491127+Meha931@users.noreply.github.com>
2022-09-12 19:12:03 +0700

    Let nginx handle this

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const website = express();
 website.set("views", root);
 website.set("view engine", "ejs");
 
+/*/ Unneeded, nginx handles extensions
 function doesFileExist(path) {
     try {
         readFileSync(path);
@@ -21,25 +22,26 @@ function getExt(path) {
     let l = path.lastIndexOf(".");
     return (l===-1) ? "" : path.slice(l+1);
 }
+//*/
 
-website.get("/*", (req, res) => {
+website.get(/^\/.*\.ejs$/, (req, res) => { // "/***.ejs"
     let p = root+req.path;
-    if (getExt(req.path) === "ejs") {
-        ejs.renderFile(p, {}, {}, (err, str) => {
-            if (err) {
-                //console.log(err);
-                res.sendStatus(400);
-                return;
-            }
-            res.send(str);
-        });
-    } else {
-        if (doesFileExist(p)) res.sendFile(__dirname+"/"+p);
-        else res.sendStatus(404);
-    }
+    console.log(p);
+    ejs.renderFile(p, {}, {}, (err, str) => {
+        if (err) {
+            //console.log(err);
+            res.sendStatus(400);
+            return;
+        }
+        res.send(str);
+    });
 });
 
-//website.use(express.static(__dirname+"/"+root));
+website.get("*", (req, res) => {
+    res.sendStatus(418); // fix nginx's regex if you'll ever encounter this teapot
+});
+
+//website.use(express.static(__dirname+"/"+root)); // sends raw .ejs as downloadable file
 
 website.listen(13377, () => {
     console.log("Ready");
